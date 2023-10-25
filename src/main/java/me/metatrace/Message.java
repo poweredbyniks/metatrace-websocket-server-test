d92f5a139addaecd5110500dabcf7b6ab13f0910
stroganov <stroganov@msk.atb.su>
2023-10-25 19:20:09 +0400

    init repo

---
+++ src/main/java/me/metatrace/Message.java
@@ -0,0 +1,15 @@
+package me.metatrace;
+
+import java.math.BigInteger;
+
+public class Message {
+    private BigInteger number;
+
+    public BigInteger getNumber() {
+        return number;
+    }
+
+    public void setNumber(BigInteger number) {
+        this.number = number;
+    }
+}
